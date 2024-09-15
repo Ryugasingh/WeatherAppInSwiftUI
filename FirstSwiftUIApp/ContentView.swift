dcfdba143e7ded2da663da7cc85eadb2eb088460
Sambhav Singh <ryugasingh90@gmail.com>
2024-09-15 19:37:28 +0530

    Initial Commit

---
+++ FirstSwiftUIApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirstSwiftUIApp
+//
+//  Created by Sambhav Singh on 15/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstSwiftUIApp/FirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIAppApp.swift
+//  FirstSwiftUIApp
+//
+//  Created by Sambhav Singh on 15/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
